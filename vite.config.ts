fa54d540c25bad4d91da3bc8c8daa6aa8dc609f3
codebyma <minimar0@naver.com>
2026-08-14 14:35:15 +0900

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// mode에 따라 base 자동 분기
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: mode === 'production' ? '/react/' : '/', // repo 이름 맞춰서 변경
+}))
